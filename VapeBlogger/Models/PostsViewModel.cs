00aabd6afb9404c7c8bff0b5fb292fbe70a8aef8
burakkilic44 <32898628+burakkilic44@users.noreply.github.com>
2017-12-21 23:29:26 +0300

    2112chngs

--- VapeBlogger/Models/PostsViewModel.cs
+++ VapeBlogger/Models/PostsViewModel.cs
@@ -9,7 +9,7 @@ namespace VapeBlogger.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string CreateDate { get; set; }
+        public DateTime CreateDate { get; set; }
         public string Photo { get; set; }
     }
 }
